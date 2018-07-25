d7aa29fb88ab7e0f226512ff8c0b5d205a384ad4
Oliver Russell <ojhrussell@gmail.com>
2018-07-25 16:06:27 +0100

    --wip-- [skip ci]

--- radslam/index.js
+++ radslam/index.js
@@ -2,52 +2,117 @@ const ebnf = require('ebnf')
 const jsYaml = require('js-yaml')
 const R = require('ramda')
 
+// TODO: specifiy:
+// > Func Arg Arg ...
+// v Header Header ...
+// ^ Header Func Arg Arg ...
+// X Arg
+// | Arg
+// - Arg
+// J Arg
+// G Header Header ...
+//     AGG_BLOCK
+// Relation
+//     BLOCK
+// Var
+//     BLOCK
+// Def Arg Arg ...
+//     BLOCK
+
+// do
+// INDENT DEDENT as tokens in the lexer like python, see python ebnf
+
+// const grammar = `
+// program              ::= NEWLINE* block+
+
+// WS                   ::= #x20+   /* " "+  */
+// NEWLINE              ::= #x0A    /* "\n" */
+// INDENT               ::= "<INDENT>" NEWLINE
+// DEDENT               ::= "<DEDENT>" NEWLINE
+
+// block                ::= INDENT (line+ | block+) NEWLINE* DEDENT
+// line                 ::= WS (relation | (operator (WS value)*) | var) (NEWLINE | EOF)
+// value                ::= literal | relation | header | var | set
+
+// set                  ::= "[" (value WS)* value "]"
+// var                  ::= [a-zA-Z_][a-zA-Z_0-9]*
+// relation             ::= [a-zA-Z_][a-zA-Z_0-9]* ":"
+// header               ::= ":" [a-zA-Z_][a-zA-Z_0-9]*
+// operator             ::= ">" | "v" | "^" | "X" | "|" | "-" | "J" | "G" | [A-Z][a-zA-Z_0-9]*
+
+// literal              ::= number | string | bool | template
+// bool                 ::= "true" | "false"
+// null                 ::= "null"
+// number               ::= "-"? ("0" | [1-9] [0-9]*) ("." [0-9]+)? (("e" | "E") ( "-" | "+" )? ("0" | [1-9] [0-9]*))?
+// string               ::= '"'  (([#x20-#x21] | [#x23-#x5B] | [#x5D-#xFFFF]) | #x5C (#x22 | #x5C | #x2F | #x62 | #x66 | #x6E | #x72 | #x74 | #x75 HEXDIG HEXDIG HEXDIG HEXDIG))* '"'
+// template             ::= '\`' (([#x20-#x5B] | [#x5D-#x5F] | [#x61-#xFFFF]) | #x5C (#x60 | #x5C | #x2F | #x62 | #x66 | #x6E | #x72 | #x74 | #x75 HEXDIG HEXDIG HEXDIG HEXDIG))* '\`'
+// HEXDIG               ::= [a-fA-F0-9]
+// `
+// // backtick is #x60
+// // quote is #x22
+// // backslash is #x5C
+// const parser = new ebnf.Grammars.W3C.Parser(grammar)
+
+let addIndents = s=>{
+    let lastIndent = 0
+    let lines = ['<INDENT>']
+    s.split('\n').forEach((line, lineNumber)=>{
+        if(line.trim() === ''){}
+        else{
+            let match = line.match(/^(    )*[^ ]/g)
+            if(!match) throw `line: ${lineNumber + 1} incorrectly indented`
+            thisIndent = (match[0].length - 1) / 4
+            if(thisIndent > lastIndent) lines.push('<INDENT>')
+            if(thisIndent < lastIndent) lines.push('<DEDENT>')
+            lastIndent = thisIndent
+        }
+        lines.push(line)
+    })
+    lines.push('<DEDENT>')
+    return lines.join('\n')
+}
+
+// const s = `a_relation:
+
+// J
+// v 5 \`bar\`
+
+// > 4 "foo"
+// > 5
+//     Custom 6 x_fooooo1
+//     J 9
+// f
+// - 7 :some_header some_relation:
+//     X "la"
+//         | "loo" [4 5 :other_table]
+
+// some_set
+// - other_set
+// `
+
+
+
 const grammar = `
-program              ::= NEWLINE* block+
+block                ::= INDENT (line | block)+ DEDENT
+line                 ::= [a-z]+ NEWLINE
 
 WS                   ::= #x20+   /* " "+  */
 NEWLINE              ::= #x0A    /* "\n" */
-indent               ::= #x20 #x20 #x20 #x20
-
-block                ::= (value_line | line)+ (NEWLINE+ | EOF)
-value_line           ::= indent* value (NEWLINE | EOF)
-line                 ::= indent* operator WS (value WS)* value (NEWLINE | EOF)
-value                ::= literal | relation | header | var | set
-
-set                  ::= "[" (value WS)* value "]"
-var                  ::= [a-zA-Z_][a-zA-Z_0-9]*
-relation             ::= [a-zA-Z_][a-zA-Z_0-9]* ":"
-header               ::= ":" [a-zA-Z_][a-zA-Z_0-9]*
-operator             ::= ">" | "v" | "^" | "X" | "|" | "-" | "J" | "G" | [A-Z][a-zA-Z_0-9]*
-
-literal              ::= number | string | bool | template
-bool                 ::= "true" | "false"
-null                 ::= "null"
-number               ::= "-"? ("0" | [1-9] [0-9]*) ("." [0-9]+)? (("e" | "E") ( "-" | "+" )? ("0" | [1-9] [0-9]*))?
-string               ::= '"'  (([#x20-#x21] | [#x23-#x5B] | [#x5D-#xFFFF]) | #x5C (#x22 | #x5C | #x2F | #x62 | #x66 | #x6E | #x72 | #x74 | #x75 HEXDIG HEXDIG HEXDIG HEXDIG))* '"'
-template             ::= '\`' (([#x20-#x5B] | [#x5D-#x5F] | [#x61-#xFFFF]) | #x5C (#x60 | #x5C | #x2F | #x62 | #x66 | #x6E | #x72 | #x74 | #x75 HEXDIG HEXDIG HEXDIG HEXDIG))* '\`'
-HEXDIG               ::= [a-fA-F0-9]
+INDENT               ::= "<INDENT>" NEWLINE
+DEDENT               ::= "<DEDENT>" NEWLINE
 `
-// backtick is #x60
-// quote is #x22
-// backslash is #x5C
+
+const s =`<INDENT>
+waa
+waa
+<INDENT>
+woo
+<DEDENT>
+<DEDENT>
+`
 const parser = new ebnf.Grammars.W3C.Parser(grammar)
+const ast = parser.getAST(s)
 
-const ast = parser.getAST(`
-
-a_relation:
-v 5 \`bar\`
-
-> 4 "foo"
-> 5
-    Custom 6 x_fooooo1
-    J 9
-
-
-- 7 :some_header some_relation:
-    X "la"
-        | "loo" [4 5 :other_table]
-`)
 
 const alwaysSingular = ['value', 'literal']
 const useful = o=>o.children.length?
@@ -60,4 +125,45 @@ const useful = o=>o.children.length?
 
 
 console.log(jsYaml.dump(useful(ast)))
-// console.log(ast)
+// let s = `
+
+// block-1
+// block-1
+
+// block-2
+// block-2
+//     block-3
+
+//     block-3
+//         block-4
+//     block-5
+//     block-5
+
+
+// block-7
+// `
+
+// expected = `
+// INDENT
+
+// block-1
+// block-1
+
+// block-2
+// block-2
+// INDENT
+// block-3
+
+// block-3
+// INDENT
+// block-4
+// DEDENT
+// DEDENT
+// block-5
+// block-5
+// DEDENT
+
+
+// block-7
+// DEDENT
+// `
