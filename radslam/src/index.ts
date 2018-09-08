b9839c5d5e0485fd20fa49fda66fb11eb9b16154
Oliver Russell <ojhrussell@gmail.com>
2018-09-08 14:21:05 +0100

    --wip-- [skip ci]

--- radslam/src/index.ts
+++ radslam/src/index.ts
@@ -1,6 +1,47 @@
-import {foo, bar} from './server'
+import { foo, bar } from './server'
+import { zip } from 'ramda'
 import * as m from 'mithril'
 
-let View = ()=>m('h1', ['yoho', foo, bar(6)])
+const pythonSource1 = `import foo
+
+d = [1, 2, 3]
+
+while len(d) > 100:
+    pass
+
+01234567890123456789012345678901234567890123456789012345678901234567890123456789
+1         2         3         4         5         6         7         8
+
+ast = (`
+const dawdleSource1 = `table:
+J another:`
+const pythonSource2 = `)
+
+and_then = (`
+const dawdleSource2 = `table:
+> value true`
+const pythonSource3 = `)`
+const dawdleInfo1 = `some
+-------
+foo
+bar`
+const allSource = [
+    [pythonSource1, ''],
+    [dawdleSource1, dawdleInfo1],
+    [pythonSource2, ''],
+    [dawdleSource2, ''],
+    [pythonSource3, ''],
+]
+
+// const OriginalBlock = (source: string)=>m('.source.pre.left', source)
+const OriginalBlock = (source: string)=>m('.source.pre.left', source, m('.cursor', ''))
+const DawdleBlock = OriginalBlock
+
+const InfoBlock = (info: string)=>m('.source.pre.right', info)
+
+const View = ()=>m('div', allSource.map(([source, info])=>
+    m('.block',
+        OriginalBlock(source),
+        InfoBlock(info))))
 
 m.mount(document.body, {view: View})
